d05addfc6dd06df681fdbf7c5f1204117bef3f93
Josh Birlingmair <jcbmair@gmail.com>
2021-05-04 18:06:22 -0600

    getAccessToken's API call now requests a content type

--- jukebox.js
+++ jukebox.js
@@ -9,7 +9,7 @@ const APIController = (function() {
             const result = await fetch('https://accounts.spotify.com/api/token', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded', 
+                    'Content-Type': 'application/x-www-form-urlencoded',
                     'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
                 },
                 body: 'grant_type=client_credentials'
@@ -26,6 +26,7 @@ const APIController = (function() {
             const result = await fetch('https://accounts.spotify.com/api/token', {
                 method: 'POST',
                 headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
                     'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
                 },
                 body: {
